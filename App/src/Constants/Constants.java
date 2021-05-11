570a912b1cd76eaf32d31d2e223b0a19d10607fd
Karl Damus <getaholdofkarl@gmail.com>
2021-05-11 17:06:09 -0400

    Update Constants.java

--- App/src/Constants/Constants.java
+++ App/src/Constants/Constants.java
@@ -3,5 +3,8 @@ package Constants;
 import java.awt.*;
 
 public interface Constants {
+    String APP_NAME = "Art With A Heart Inc | Art Therapy Cards";
     Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
+    Dimension MIN_SIZE = new Dimension(1658, 651);
+    int NUMBER_OF_CARDS = 8;
 }
