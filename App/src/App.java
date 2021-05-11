039dba6c12cc8d4b971dc51b6dcbfe9bb404a173
Karl Damus <getaholdofkarl@gmail.com>
2021-05-11 17:06:18 -0400

    Update App.java

--- App/src/App.java
+++ App/src/App.java
@@ -1,29 +1,36 @@
-import Components.Card;
+/**
+ * App.java
+ * The entry point for this application.
+ * @author Karl Damus
+ */
+
 import UI.Window;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 import Constants.Constants;
 
 public class App implements Constants {
-    private static String[] ranks = new String[] {
-        "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
-    };
-    private static String[] suits = new String[] {
-        "C", "S", "D", "H"
-    };
-    private static Random rand = new Random();
 
     public static void main(String[] args) {
         Window window = new Window();
-        Card card = new Card(generateRandomCard(), true);
+        window.displayBacksOfCards();
+        window.displayButtons();
 
-//        window.add(card);
+//        displayWindowSize(window);
     }
 
-    private static String generateRandomCard() {
-        int rank = rand.nextInt(ranks.length);
-        int suit = rand.nextInt(suits.length);
+    // testing
 
-        return ranks[rank] + suits[suit];
+    public static void displayWindowSize(Window window) {
+        while (true) {
+            System.out.println(window.getWindow().getSize());
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
